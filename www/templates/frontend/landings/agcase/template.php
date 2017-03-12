4b7b58922904e2bdd394f7e4723e4964f2e359d4
novichkovv <novichkovv@bk.ru>
2017-03-12 13:41:46 +0700

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

---
+++ www/templates/frontend/landings/agcase/template.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<!-- saved from url=(0040)http://reals-gooods.ru/antigravitycase1/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>เคสโทรศัพท์ต้านแรงโน้มถ่วง</title>
+    
+    <meta name="viewport" content="width=480">
+    <meta name="description" content="ต่อต้านแรงโน้มถ่วงAdsorbableกรณีโทรศัพท์สำหรับiPhone 6/6วินาทีกรณีMagicalนาโนดูดเย็นต้านแรงโน้มถ่วงปกรายละเอียดสินค้า">
+    <meta name="keywords" content="ต่อต้านแรงโน้มถ่วงAdsorbableกรณีโทรศัพท์สำหรับiPhone 6/6วินาทีกรณีMagicalนาโนดูดเย็นต้านแรงโน้มถ่วงปก">
+    
+    <link rel="icon" href="http://reals-gooods.ru/antigravitycase1/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="http://reals-gooods.ru/antigravitycase1/favicon.ico" type="image/x-icon">
+
+    <link href="<?php echo $dir; ?>css/css.css" rel="stylesheet">
+    <link media="all" href="<?php echo $dir; ?>css/main.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="<?php echo $dir; ?>js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo $dir; ?>js/init.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo $dir; ?>css/roboto.css">
+    <script src="<?php echo $dir; ?>js/jquery.js" type="text/javascript"></script>
+    <script src="<?php echo $dir; ?>js/plugins.js" type="text/javascript"></script>
+    <script src="<?php echo $dir; ?>js/detect.js" type="text/javascript"></script>
+    <script src="<?php echo SITE_DIR; ?>js/common/common.js" type="text/javascript"></script>
+</head>
+    <body>
+            <div class="main-wrap">
+            <div class="section block-1">
+                <div class="container clearfix">
+                    <div class="models">
+                        <h4>เหมาะสำหรับ</h4>
+                        <ul>
+                            <li>iPhone 5</li>
+                            <li>iPhone 5s</li>
+                            <li>iPhone 6</li>
+                            <li>iPhone 6s</li>
+                        </ul>
+                    </div>
+                    <div class="cost">
+                        <h4>โปรโมชัน:</h4>
+                        <span class="sale">-70%</span><br>
+                        <span class="old-cost">2640฿</span><br>
+                        <span class="new-cost">790฿</span>
+                    </div>
+                </div>
+            </div>
+            <div class="center but-1"><a href="#delivery" class="button-m"></a></div>
+            <div class="section block-2">
+                <h2>สิ่งประดิษฐ์ปี2017</h2>
+                <div class="container">
+                    <p>เคสต้านแรงโน้มถ่วงสามารถเพิ่มความสามารถในการใช้งานของโทรศัพท์คุณได้หลายเท่าและทำให้ไม่มีสิ่งใดแทนที่มันได้ในชีวิตประจำวันสาเหตุที่มันทำเช่นนี้ได้เนื่องจากมีการนำเทคโนโลยี"การติด"เข้ามาใช้ดังนั้นเคสจึงสามารถติดกับพื้นผิวใดๆก็ได้แม้จะเป็นพื้นผิวที่ลื่นที่สุดก็ตาม! นอกจากนั้นยังมีชั้นป้องกันการสั่นสะเทือนพิเศษสำหรับการปกป้องโทรศัพท์ของคุณอีกด้วย</p>
+                    <img src="<?php echo $dir; ?>img/bg-3.jpg" alt="เคสโทรศัพท์ต้านแรงโน้มถ่วง">
+                    <p>รับประกันการยึดติดที่เชื่อถือได้เข้ากับพื้นผิวอย่างเรียบลื่นเนื่องจากมีการใช้วัสดุพิเศษที่มีรูพรุนสำหรับฝาครอบด้านหลังในกรณีที่มีการสัมผัสหรือแรงกดมันจะทำงานเหมือนเป็นถ้วยดูดขนาดเล็กมากหลายพันอันจึงทำให้คุณสามารถถืออุปกรณ์ได้แต่ตัวแผงนั้นไม่เหนียวหนึบเลยแม้แต่น้อยจึงสามารถขจัดความไม่สะดวกในการใช้ในชีวิตประจำวันออกไปได้</p>
+                </div>
+            </div>
+            <div class="section block-3">
+                <h2>BEST OF 2017!</h2>
+                <div class="container">
+                    <ul>
+                        <li>ทนต่อแรงกระแทกได้สูงไม่ได้รับผลกระทบจากความเสียหายหรือรอยขีดข่วนเล็กน้อย</li>
+                        <li>ไม่มีผลกระทบต่อโมดูลการสื่อสารของโทรศัพท์</li>
+                        <li>มีสไตล์และบางเฉียบ-เพิ่มขนาดอุปกรณ์เพียงแค่ประมาณ3มม.</li>
+                    </ul>
+                    <div class="center"><a href="#delivery" class="button-m"></a></div>
+                </div>
+            </div>
+            <div class="section block-4">
+                <h2>เหมาะสำหรับ:</h2>
+            </div>
+            <div class="section block-7">
+                <h2>การจัดส่งและการชำระเงิน</h2>
+                <div class="container">
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-1"></span>
+                        <p>ตัวเลือกการจัดส่ง:ไปรษณีย์หรือจดหมายชั้น1เวลาการจัดส่ง: 1 -3วันทำการค่าจัดส่ง: 100฿</p>
+                    </div>
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-2"></span>
+                        <p>จ่ายเงินโดยตรงเมื่อได้รับสินค้าที่สั่ง(หลังการจัดส่ง)<!-- или онлайн. При оплате онлайн предоставляется скидка 300 рублей.--></p>
+                    </div>
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-3"></span>
+                        <p>คุณสามารถคืนสินค้าได้ภายใน14วันหลังการจัดส่งไม่ว่าจะด้วยเหตุผลใดในการคืนสินค้าก็ตาม</p>
+                    </div>
+                </div>
+                <div class="center"><a href="#delivery" class="button-m"></a></div>
+            </div>
+            <!--<div class="section block-5">
+                <h2>Варианты расцветки</h2>
+            </div>-->
+            <div class="section block-1 end">
+                <div class="container clearfix">
+                    <div class="models">
+                        <h4>เหมาะสำหรับi</h4>
+                        <ul>
+                            <li>iPhone 5</li>
+                            <li>iPhone 5s</li>
+                            <li>iPhone 6</li>
+                            <li>iPhone 6s</li>
+                        </ul>
+                    </div>
+                    <div class="cost">
+                        <h4>โปรโมชัน:</h4>
+                        <span class="sale">-70%</span><br>
+                        <span class="old-cost">2460฿</span><br>
+                        <span class="new-cost">790฿</span>
+                    </div>
+                </div>
+                <div class="container-2" id="delivery">
+                    <div class="order">
+                        <p class="center">
+                            <strong>ทำการร้องขอ</strong><br>
+                            แล้วเราจะติดต่อคุณเพื่อหารือเกี่ยวกับรายละเอียดการสั่งซื้อของคุณ
+                        </p>
+                        <form action="<?php echo SITE_DIR; ?>checkout/success/" method="post" class="m1_form">
+                            <div><input type="text" name="user[user_name]" placeholder="ชื่อของคุณ" required=""></div>
+                            <div><input type="text" id="phone" name="user[phone]" placeholder="หมายเลขโทรศัพท์ของคุณ" required=""></div>
+                            <input type="hidden" name="visitor_id" value="<?php echo $visitor_id; ?>"/>
+                            <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>"/>
+                            <input type="hidden" name="s" value="<?php echo $_GET['s']; ?>">
+                            <input type="hidden" name="t" value="<?php echo $_GET['t']; ?>">
+                            <input type="hidden" name="w" value="<?php echo $_GET['w']; ?>">
+                            <input type="hidden" name="pixel" value="<?php echo $_GET['pixel']; ?>">
+                            <div><button class="button-m"></button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+              
+        </div>
+        <script type="text/javascript">
+            $ = jQuery.noConflict();
+            $(document).ready(function () {
+                $("#phone").focus(function() {
+                    if ($(this).val() == '') {
+                        $(this).val('+66');
+                    }
+                });
+                $("#phone").mask("+669999999999");
+            });
+        </script>
+    </body>
+</html>
